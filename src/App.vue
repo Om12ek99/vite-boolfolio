ce82524d4f3dc13eb9ac528f6008f5cb77cd8f66
Om12ek99 <omar.elkamili@gmail.com>
2024-06-28 23:52:33 +0200

    add: added component Appmain and tested

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
-export  default{
-
+import AppMain from "./components/AppMain.vue";
+export default {
+  components: {
+    AppMain,
+  }
 }
 </script>
 
 <template>
- <h1>ciao mondo</h1>
+  <h1>ciao mondo</h1>
+  <AppMain />
 </template>
-
-
---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from "axios";
+export  default{
+    data() {
+        return {
+            //array vuoto inizializato
+            projects: [],
+        };
+    },
+    created() {
+        axios.get("http://127.0.0.1:8000/api/projects").then((resp)=> {
+            console.log(resp);
+        })
+    },
+}
+</script>
+<template>
+        <h2>ciao</h2>
+</template>
+<style>
+</style>
