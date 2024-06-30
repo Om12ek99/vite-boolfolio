16633223ae4bbc630b323ea84992e194c22bcaeb
Om12ek99 <omar.elkamili@gmail.com>
2024-06-30 23:53:31 +0200

    feat: added images display

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,6 +10,7 @@ export default {
         return {
             //array vuoto inizializato
             projects: [],
+            
         };
     },
     created() {
--- src/components/AppProjectCard.vue
+++ src/components/AppProjectCard.vue
@@ -5,14 +5,24 @@ export default {
         project: {
             type: Object,
             required: true
+        },
+        image: String,
+    },
+    data() {
+        return {
+            imageBaseUrl: 'http://127.0.0.1:8000/storage'
         }
     }
 }
+
 </script>
 
 <template>
     <div class="card h-100">
         <div class="card-body">
+            <img class="card-image" :src="project.cover_image
+                    ? `${imageBaseUrl}/${project.cover_image}`
+                    : `https://placehold.co/500x300?text=immagine+non+disponibile`" alt="cover_image">
             <h5 class="card-title">{{ project.title }}</h5>
             <p class="card-text">{{ project.content }}</p>
         </div>
